af5587d14bacffe28d77179f780cbcd7faac11e9
DamithaNeranjan <41992701+DamithaNeranjan@users.noreply.github.com>
2023-06-18 10:55:17 +0530

    Final Score Calculation Done.

--- test_model.py
+++ test_model.py
@@ -34,7 +34,25 @@ emotions = {
 }
 
 # Emotions to observe
-observed_emotions = ['calm', 'happy', 'fearful', 'neutral', 'surprised']
+observed_emotions = ['angry',
+                     'happy',
+                     'fearful',
+                     'neutral',
+                     'surprised',
+                     'sad']
+
+emotionWeightages = {
+    "angry": 0.52,
+    "happy": 1.53,
+    "fearful": 0.77,
+    "neutral": 1,
+    "surprise": 0.62,
+    "sad": 0.43,
+}
+
+audioScore = 0.0
+finalAudioScore = 0.0
+audioFileCount = 0
 
 
 # Load the data and extract features for each sound file
@@ -67,7 +85,7 @@ def extract_feature(file_name, mfcc, chroma, mel):
             chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
             result = np.hstack((result, chroma))
         if mel:
-            mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T, axis=0)
+            mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T, axis=0)
             result = np.hstack((result, mel))
     return result
 
@@ -75,15 +93,70 @@ def extract_feature(file_name, mfcc, chroma, mel):
 filename = 'modelForPrediction1.sav'
 loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
 
-feature1 = extract_feature(
-    # "D:\\L4S1\\Research Project\\Interim\\Video Sample\\DamithaAudio1.mp3",
-    "speech_audio/DamithaAudio1Mono.wav",
-    # "speech_audio/Actor_01/03-01-01-01-01-01-01.wav",
-    # "speech_audio/03-01-01-01-01-01-01.wav",
-    mfcc=True, chroma=True, mel=True)
+# Set the path to the folder containing the WAV files
+folder_path = "D:\\L4S1\\Research Project\\Final\\Candidate Videos\\6 Candidates Interview audio\\Candidate1\\Converted"
 
-feature1 = feature1.reshape(1, -1)
+# Iterate over each file in the folder
+for file_name in os.listdir(folder_path):
+    if file_name.endswith(".wav"):
+        # Construct the full path to the WAV file
+        file_path = os.path.join(folder_path, file_name)
 
-prediction = loaded_model.predict(feature1)
+        # Extract features for the current WAV file
+        features = extract_feature(file_path, mfcc=True, chroma=True, mel=True)
+        features = features.reshape(1, -1)
 
-print(prediction)
+        # Make a prediction using the loaded model
+        prediction = loaded_model.predict(features)
+
+        audioFileCount = audioFileCount + 1
+
+        if prediction == 'calm':
+            prediction = ['neutral']
+        elif prediction == 'disgust':
+            prediction = ['fearful']
+
+        if prediction == 'angry':
+            audioScore = audioScore + emotionWeightages['angry']
+            print('angry')
+        elif prediction == 'fearful':
+            audioScore = audioScore + emotionWeightages['fearful']
+            print('fearful')
+        elif prediction == 'happy':
+            audioScore = audioScore + emotionWeightages['happy']
+            print('happy')
+        elif prediction == 'neutral' or prediction == ['neutral']:
+            audioScore = audioScore + emotionWeightages['neutral']
+            print('neutral')
+        elif prediction == 'sad':
+            audioScore = audioScore + emotionWeightages['sad']
+        elif prediction == 'surprise':
+            audioScore = audioScore + emotionWeightages['surprise']
+
+        # Do something with the prediction
+        print(f"File: {file_name}, Prediction: {prediction}, Current Score: {audioScore}")
+
+print(f"Before Final Score: {round(audioScore, 2)}")
+
+finalAudioScore = (audioScore/audioFileCount)*100
+
+print(f"Calculated Final Score: {round(finalAudioScore, 2)}")
+
+# feature1 = extract_feature(
+#     # "D:\\L4S1\\Research Project\\Interim\\Video Sample\\DamithaAudio1.mp3",
+#     # "speech_audio/DamithaAudio1Mono.wav",
+#     "D:\\L4S1\\Research Project\\Final\\Candidate Videos\\6 Candidates Interview audio\\Candidate6\\Converted\\66.wav",
+#     # "speech_audio/Actor_01/03-01-01-01-01-01-01.wav",
+#     # "speech_audio/03-01-01-01-01-01-01.wav",
+#     mfcc=True, chroma=True, mel=True)
+#
+# feature1 = feature1.reshape(1, -1)
+#
+# prediction = loaded_model.predict(feature1)
+#
+# if prediction == 'calm':
+#     prediction = 'neutral'
+# elif prediction == 'disgust':
+#     prediction = 'fearful'
+#
+# print(prediction)
